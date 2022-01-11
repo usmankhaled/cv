1b489fc552b9722b8ab4f8b4018b253b817aac88
usmankhalid1 <uk0663@gmail.com>
2022-01-11 23:25:21 +0500

    add face code in js

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function moveoval(){
 
 }
  const lov= setInterval(moveoval,1000);
+// code for face change
+
+ const face=document.querySelector('.mouth')
+ function control(e){
+     if(e.key==='ArrowRight'){
+         face.classList.remove('mouth')
+        face.classList.add('mouth-sad')
+     }
+     else if(e.key==='ArrowLeft'){
+         face.classList.remove('mouth-sad')
+         face.classList.add('mouth')
+     }
+ }
+ document.addEventListener('keydown',control)
